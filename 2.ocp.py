990f9ef01c0af52f7350c1955f4ad06482bd8557
Mateus Grellert <mateus.grellert@ufsc.br>
2020-11-02 22:31:33 -0300

    up

---
+++ 2.ocp.py
@@ -0,0 +1,47 @@
+"""
+Open-Closed Principle
+
+Classes devem estar fechadas para modificação, mas abertas para extensão
+"""
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def get_name(self) -> str:
+        pass
+
+animals = [
+    Animal('lion'),
+    Animal('mouse')
+]
+
+def animal_sound(animals: list):
+    for animal in animals:
+        if animal.name == 'lion':
+            print('roar')
+
+        elif animal.name == 'mouse':
+            print('squeak')
+
+animal_sound(animals)
+
+
+"""
+Outro exemplo:
+
+Imagine que você tem uma loja que dá desconto de 20% aos seus clientes favoritos
+usando essa classe abaixo. Quando você decide dar 40% de desconto a clientes VIP,
+você decide mudar a classe da seguinte forma:
+"""
+
+class Discount:
+    def __init__(self, customer, price):
+        self.customer = customer
+        self.price = price
+
+    def give_discount(self):
+            if self.customer == 'fav':
+                return self.price * 0.2
+            if self.customer == 'vip':
+                return self.price * 0.4
+
---
+++ 4.isp.py
@@ -0,0 +1,46 @@
+"""
+Interface Segregation Principle
+Crie interfaces que são específicas. Clientes não devem depender de interfaces que eles não usarão
+"""
+
+class IShape:
+    def draw_square(self):
+        raise NotImplementedError
+    
+    def draw_rectangle(self):
+        raise NotImplementedError
+    
+    def draw_circle(self):
+        raise NotImplementedError
+
+
+class Circle(IShape):
+    def draw_square(self):
+        pass
+
+    def draw_rectangle(self):
+        pass
+    
+    def draw_circle(self):
+        pass
+
+class Square(IShape):
+    def draw_square(self):
+        pass
+
+    def draw_rectangle(self):
+        pass
+    
+    def draw_circle(self):
+        pass
+
+class Rectangle(IShape):
+    def draw_square(self):
+        pass
+
+    def draw_rectangle(self):
+        pass
+    
+    def draw_circle(self):
+        pass
+
